5396bc7ca21c2330b651e9c308e878a036485f95
Arif <faizin.arief@gmail.com>
2020-05-27 13:52:46 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/mysimpledagger/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.dicoding.mysimpledagger
+
+import android.app.Application
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import javax.inject.Inject
+
+@Module
+class AppModule {
+    @Provides
+    fun provideEngine(): Engine = Engine()
+}
+
+@Component(modules = [AppModule::class])
+interface AppComponent {
+    @Component.Factory
+    interface Factory {
+        fun create(): AppComponent
+    }
+
+    fun inject(activity: MainActivity)
+}
+
+open class MyApplication : Application(){
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create()
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var engine: Engine
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        (application as MyApplication).appComponent.inject(this)
+
+        val car = Car(engine)
+        car.start()
+    }
+}
+
+class Car(private val engine: Engine) {
+    fun start() {
+        engine.start()
+    }
+}
+
+class Engine {
+    fun start() {
+        println("Engine started....")
+    }
+}
